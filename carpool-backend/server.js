28e542818d2f2eb986da7f89972e3b593d8a49ee
jimmy-oss <jimmynjonge51@gmail.com>
2025-04-28 20:11:32 +0300

    mongodb configurations

--- carpool-backend/server.js
+++ carpool-backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 
 dotenv.config();
 
@@ -20,3 +22,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully ✅'))
+  .catch((err) => {
+    console.error('MongoDB connection error ❌:', err.message);
+  });
+
